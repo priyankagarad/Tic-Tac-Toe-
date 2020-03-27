7b4c388ca386001ca808a891ea4445c61e48488d
priyankagarad <garadpriyanka1995@gmail.com>
2020-03-27 20:55:38 +0530

    Added:assigned value to user

--- ticTacToe.sh
+++ ticTacToe.sh
@@ -2,13 +2,31 @@
 echo "Welcome on Tic Tac Toe Game"
 
 declare -a board
-# Reset board to start play
 
+# display board
+function board() {
 board=(1 2 3 4 5 6 7 8 9)
-
-# To display board
 for (( i=0;i<9;i=i+3 ))
 do
 		echo " |${board[$i]} | ${board[$i+1]} | ${board[$i+2]} |"
 		echo "             "
 done
+}
+
+#assign value to user
+function assignSymbol()
+{
+	symbol=$((RANDOM%2))
+		if [[ $symbol -eq 1 ]]
+		then
+				player="user"
+				user="X"
+				computer="O"
+		else
+				player="computer"
+				user="O"
+				computer="X"
+		fi
+}
+board
+assignSymbol
